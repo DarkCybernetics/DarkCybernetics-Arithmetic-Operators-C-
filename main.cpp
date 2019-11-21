d8c472ad54d164c44693321bf386d424cdfb13f8
DarkCybernetics <Dion.jacksoncs@yahoo.com>
2019-11-21 09:40:53 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+/*
+======================================
+Arithmetic Operators: + - * / %
+Incremental  ++
+Decremental --
+Assignment =
+--------------------------------------
+All Rights Reserved 2019.
+www.DarkCybernetics.com
+*/
+using namespace std;
+
+int main()
+{
+    int solution =0;
+    int a_number =0;
+    int b_number =0;
+    cout<<" A value :"<<endl;
+    cin>>a_number;
+    cout<<" B value :"<<endl;
+    cin>>b_number;
+// Addition
+//solution = a_number + b_number;
+//Subtraction
+//solution = a_number - b_number;
+//Multiplication
+//solution = a_number * b_number;
+//Division
+//solution = a_number / b_number;
+//Modulus
+solution = a_number % b_number;
+cout<<"Answer :"<<solution;
+ return 0;
+}
